df0a6738d7dd0ace5235cf091d160daff3f27a4d
yanminsk <yanminsk@tut.by>
2023-11-07 23:18:26 +0300

    sdd .idea

--- 3_2.py
+++
@@ -1,6 +0,0 @@
-number=input("Введите числа : ").strip()
-number=number.split()
-sum_number=0
-for i in range(len(number)):
-    sum_number+=int(number[i])
-print("Среднее арифмитическое = ",round(sum_number/len(number),3))
