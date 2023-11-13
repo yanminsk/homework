f01850be02b0356d63a9e02e16667df46060b5f6
yanminsk <yanminsk@tut.by>
2023-11-13 18:35:57 +0300

    count simbols from keyboard 2 generation

--- 4_2.py
+++ 4_2.py
@@ -1,4 +1,10 @@
+# my_string=input("Введите текст:")
+# my_spisok=list(my_string)
+# my_dict={i: my_spisok.count(i) for i in my_string}
+# print(my_dict)
+
 my_string=input("Введите текст:")
-my_spisok=list(my_string)
-my_dict={i: my_spisok.count(i) for i in my_string}
+# my_spisok=list(my_string)
+my_dict={}
+my_dict={i: int(my_dict.get(i,0))+1 for i in my_string}
 print(my_dict)
