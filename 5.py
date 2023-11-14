7285107a89457d95fd5f9dada905fea0ccc519f6
yanminsk <yanminsk@tut.by>
2023-11-14 21:31:30 +0300

    classroom

---
+++ 5.py
@@ -0,0 +1,18 @@
+
+
+# number=int(input("Введите сумму"))
+# procent=int(input("Введите процент"))
+# result=number*2
+# let=0
+# while number<result :
+#     number=number+number/100*procent
+#     let +=1
+#
+# print(let)
+my_dict={}
+for _ in range(10):
+    text=input("Текст: ")
+    my_dict=text.split()
+
+
+    print(my_dict)
