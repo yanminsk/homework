9ace1281cbbabcf0433e0b083f588878a2d5b41e
yanminsk <yanminsk@tut.by>
2023-11-14 22:25:49 +0300

    calculator

---
+++ 5_2.py
@@ -0,0 +1,23 @@
+try:
+    first_digit=int(input("Введите первое число: "))
+    arithmetic=input("Введите арифметическую операцию вида (+ - / * %): ")
+    second_digit=int(input("Введите второе число: "))
+except ValueError as e:
+    print(f"Ошибка с типом {e}")
+
+result=0
+
+match arithmetic:
+    case "+":
+        result=first_digit+second_digit
+    case "-":
+        result = first_digit - second_digit
+    case "/":
+        result = first_digit / second_digit
+    case "*":
+        result = first_digit * second_digit
+    case "%":
+        result = first_digit % second_digit
+
+if result: print(f"Результат операции \"{arithmetic}\" равен", result)
+else: print("Неподдерживаемая операция")
