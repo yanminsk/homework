a826a261730c05f9d7b2d623d96212d110a39473
yanminsk <yanminsk@tut.by>
2023-11-19 23:59:28 +0300

    summa sosedei

--- 6_6.py
+++ 6_6.py
@@ -1,8 +1,16 @@
 
 mytxt=list(input("Введите список: "))
-mytxt.sort()
-result = list(filter(lambda x: int(x) % 2, mytxt))
-for i in result:
-    mytxt.append(i)
-    mytxt.remove(i)
-print(mytxt)
+my_summa=[]
+yu=len(mytxt)
+for i in range(len(mytxt)):
+    if i==0:
+        k=len(mytxt)
+        l=i
+    elif i==len(mytxt)-1:
+        k=i
+        l=-1
+    else: k=l=i
+
+    my_summa.append(int(mytxt[k-1])+int(mytxt[l+1]))
+
+print(my_summa)
