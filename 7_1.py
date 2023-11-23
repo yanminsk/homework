f82c71dcccfb1a3460b7374ee9965f3670ad86d7
yanminsk <yanminsk@tut.by>
2023-11-23 20:02:09 +0300

    cars

--- 7_1.py
+++ 7_1.py
@@ -18,28 +18,31 @@ class Car:
 
     def __str__(self):
         info = (f"Машина " + self.color + f" цвета, детское кресло "
-                + str(self.is_baby_seat)+f", мест для сидения " + str(self.count_passenger_seats))
+                + str(self.is_baby_seat)+f", количество мест " + str(self.count_passenger_seats))
         return info
 
 
 my_car1 = Car("Red", 5, True)
 my_car2 = Car("Blue", 3, False)
+my_car3 = Car("Black", 5, True)
 
-my_car=[my_car1]+[my_car2]
-
-# print(car)
-
+my_car=[my_car1]+[my_car2]+[my_car3]
 
 class Taxi:
     def __init__(self, cars):
         self.cars = my_car
 
     def find_car(self, count_passengers:int, is_baby:bool):
-        if count_passengers<self.cars.
-            pass
+        for i in range(len(self.cars)):
+            if not self.cars[i].is_busy:
+                if count_passengers<=int(self.cars[i].count_passenger_seats)\
+                    and (is_baby and is_baby==self.cars[i].is_baby_seat\
+                    or not is_baby):
+                    self.cars[i].is_busy = True
+                    return self.cars[i]
 
-        return self
+        return None
+poisk_car=Taxi(my_car)
 
-mmm=Taxi(my_car)
-
-mmm.find_car(5,True)
+print(poisk_car.find_car(5,True))
+print(poisk_car.find_car(5,False))
